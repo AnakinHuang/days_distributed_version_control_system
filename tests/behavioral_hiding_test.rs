1b14efa1e3c922d2a5f3e4804be9874b12ad6a5f
AlvinJ404 <alvinjiang01@hotmail.com>
2024-11-15 02:11:12 -0500

    test: all unit tests defined and passed for B.2.1 Command Parser

--- tests/behavioral_hiding_test.rs
+++ tests/behavioral_hiding_test.rs
@@ -1,6 +1,9 @@
 // days/tests/behavioral_hiding_test.rs
 //
 // Unit tests for A.2 Behavioral Hiding
+//
+// Author: Yifan (Alvin) Jiang
+// Date: 11/14/2024
 
 use days_dvcs::behavioral_hiding::command_parser::{parse_command, ValidCommand};
 
@@ -9,26 +12,63 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_init_command() {
+    fn test_parse_init() {
         let args = vec!["days".to_string(), "init".to_string()];
         let command = parse_command(args).unwrap();
-        assert_eq!(command, ValidCommand::Init);
+        assert_eq!(command, ValidCommand::Init {directory: Some(".".to_string())});
     }
 
     #[test]
-    fn test_parse_clone_command() {
-        let args = vec!["days".to_string(), "clone".to_string(), "https://example.com/repo.git".to_string()];
+    fn test_parse_init_with_directory() {
+        let args = vec![
+            "days".to_string(),
+            "init".to_string(),
+            "-d".to_string(),
+            "/path/to/repo".to_string(),
+        ];
         let command = parse_command(args).unwrap();
         assert_eq!(
             command,
-            ValidCommand::Clone {
-                remote_url: "https://example.com/repo.git".to_string()
+            ValidCommand::Init {
+                directory: Some("/path/to/repo".to_string())
             }
         );
     }
 
     #[test]
-    fn test_parse_add_command() {
+    fn test_parse_clone() {
+        let args = vec!["days".to_string(), "clone".to_string(), "https://example.com/repo.git".to_string()];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Clone {
+                remote_url: "https://example.com/repo.git".to_string(),
+                directory: Some(".".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_clone_with_directory() {
+        let args = vec![
+            "days".to_string(),
+            "clone".to_string(),
+            "https://example.com/repo.git".to_string(),
+            "--dir".to_string(),
+            "/path/to/clone".to_string(),
+        ];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Clone {
+                remote_url: "https://example.com/repo.git".to_string(),
+                directory: Some("/path/to/clone".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_add() {
         let args = vec!["days".to_string(), "add".to_string(), "file.txt".to_string()];
         let command = parse_command(args).unwrap();
         assert_eq!(
@@ -40,7 +80,7 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_remove_command() {
+    fn test_parse_remove() {
         let args = vec!["days".to_string(), "remove".to_string(), "file.txt".to_string()];
         let command = parse_command(args).unwrap();
         assert_eq!(
@@ -52,21 +92,21 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_status_command() {
+    fn test_parse_status() {
         let args = vec!["days".to_string(), "status".to_string()];
         let command = parse_command(args).unwrap();
         assert_eq!(command, ValidCommand::Status);
     }
 
     #[test]
-    fn test_parse_heads_command() {
+    fn test_parse_heads() {
         let args = vec!["days".to_string(), "heads".to_string()];
         let command = parse_command(args).unwrap();
         assert_eq!(command, ValidCommand::Heads);
     }
 
     #[test]
-    fn test_parse_diff_command() {
+    fn test_parse_diff() {
         let args = vec!["days".to_string(), "diff".to_string(), "rev1".to_string(), "rev2".to_string()];
         let command = parse_command(args).unwrap();
         assert_eq!(
@@ -76,5 +116,156 @@ mod tests {
                 revision_2: "rev2".to_string()
             }
         );
+    }
+
+    #[test]
+    fn test_parse_cat() {
+        let args = vec!["days".to_string(), "cat".to_string(), "file.txt".to_string()];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Cat {
+                file: "file.txt".to_string()
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_checkout() {
+        let args = vec!["days".to_string(), "checkout".to_string(), "branch".to_string()];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Checkout {
+                branch: "branch".to_string()
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_commit() {
+        let args = vec!["days".to_string(), "commit".to_string(), "message".to_string()];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Commit {
+                message: "message".to_string()
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_log() {
+        let args = vec!["days".to_string(), "log".to_string()];
+        let command = parse_command(args).unwrap();
+        assert_eq!(command, ValidCommand::Log);
+    }
+
+    #[test]
+    fn test_parse_merge() {
+        let args = vec!["days".to_string(), "merge".to_string(), "branch".to_string()];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Merge {
+                branch: "branch".to_string(),
+                directory: Some(".".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_merge_with_directory() {
+        let args = vec![
+            "days".to_string(),
+            "merge".to_string(),
+            "feature-branch".to_string(),
+            "-d".to_string(),
+            "/path/to/repo".to_string(),
+        ];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Merge {
+                branch: "feature-branch".to_string(),
+                directory: Some("/path/to/repo".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_pull() {
+        let args = vec!["days".to_string(), "pull".to_string()];
+        let command = parse_command(args).unwrap();
+        assert_eq!(command, ValidCommand::Pull {directory: Some(".".to_string())});
+    }
+
+    #[test]
+    fn test_parse_pull_with_directory() {
+        let args = vec![
+            "days".to_string(),
+            "pull".to_string(),
+            "--dir".to_string(),
+            "/path/to/repo".to_string(),
+        ];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Pull {
+                directory: Some("/path/to/repo".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_push() {
+        let args = vec!["days".to_string(), "push".to_string(), "branch".to_string()];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Push {
+                branch: Some("branch".to_string()),
+                directory: Some(".".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_push_no_branch_arg() {
+        let args = vec!["days".to_string(), "push".to_string()];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Push {
+                branch: Some("main".to_string()),
+                directory: Some(".".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_push_with_directory() {
+        let args = vec![
+            "days".to_string(),
+            "push".to_string(),
+            "branch".to_string(),
+            "-d".to_string(),
+            "/path/to/repo".to_string(),
+        ];
+        let command = parse_command(args).unwrap();
+        assert_eq!(
+            command,
+            ValidCommand::Push {
+                branch: Some("branch".to_string()),
+                directory: Some("/path/to/repo".to_string())
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_invalid() {
+        let args = vec!["days".to_string(), "invalid".to_string()];
+        let command = parse_command(args);
+        assert!(command.is_err());
     }
 }
