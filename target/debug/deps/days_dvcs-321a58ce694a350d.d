18a0ecaa5574134a296021d20d3812d3ddb1268d
Yuesong Huang <yhu116@u.rochester.edu>
2024-11-20 16:00:13 -0500

    Made checkout(...) work correctly.

---
+++ target/debug/deps/days_dvcs-321a58ce694a350d.d
@@ -0,0 +1,8 @@
+/Users/yuesong.huang/RustroverProjects/days_dvcs/target/debug/deps/libdays_dvcs-321a58ce694a350d.rmeta: src/main.rs src/behavior_hiding.rs src/file_system_hiding.rs src/repository_hiding.rs
+
+/Users/yuesong.huang/RustroverProjects/days_dvcs/target/debug/deps/days_dvcs-321a58ce694a350d.d: src/main.rs src/behavior_hiding.rs src/file_system_hiding.rs src/repository_hiding.rs
+
+src/main.rs:
+src/behavior_hiding.rs:
+src/file_system_hiding.rs:
+src/repository_hiding.rs:
---
+++ target/debug/deps/days_dvcs-80eaa189772f1498.d
@@ -0,0 +1,8 @@
+/Users/yuesong.huang/RustroverProjects/days_dvcs/target/debug/deps/libdays_dvcs-80eaa189772f1498.rmeta: src/main.rs src/behavior_hiding.rs src/file_system_hiding.rs src/repository_hiding.rs
+
+/Users/yuesong.huang/RustroverProjects/days_dvcs/target/debug/deps/days_dvcs-80eaa189772f1498.d: src/main.rs src/behavior_hiding.rs src/file_system_hiding.rs src/repository_hiding.rs
+
+src/main.rs:
+src/behavior_hiding.rs:
+src/file_system_hiding.rs:
+src/repository_hiding.rs:
